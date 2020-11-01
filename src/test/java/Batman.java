44a308724bae240c97aadcb48186d50190144a65
JHY34 <hamityilmaz34@gmail.com>
2020-11-01 09:55:23 -0600

    more updates Batman class from the gotham branch

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,5 +2,6 @@ public class Batman {
     public static void main(String[] args) {
         System.out.println("Batman class");
         // this updates from the gotham branch
+        // more changes from the gotham branch
     }
 }
