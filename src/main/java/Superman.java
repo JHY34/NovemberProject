d4f1f5b507eff3f3ee84eaf45270de92ee96eaea
JHY34 <hamityilmaz34@gmail.com>
2020-11-01 09:54:55 -0600

    updates Batman class from the gotham branch

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -1,6 +1,6 @@
 public class Superman {
     public static void main(String[] args) {
-        System.out.println("Superman");
+        System.out.println("Superman..");
         // this is updated online
     }
 }
